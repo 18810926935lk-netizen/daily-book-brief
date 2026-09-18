8afb36f7fbc0fdb7c9a31587816f252cfdd1db5b
18810926935lk-netizen <18810926935lk@gmail.com>
2026-09-18 09:35:55 +0800

    feat: sections 3->10 items + topic picks: make_docx.py

--- make_docx.py
+++ make_docx.py
@@ -3,13 +3,20 @@
 用法: python make_docx.py <输入.html> <输出.docx>
 """
 import sys
-from htmldocx import Htmldocx
+from docx import Document
 
 def main():
     html = open(sys.argv[1], encoding='utf-8').read()
-    h = Htmldocx()
-    h.add_html(html)
-    h.doc.save(sys.argv[2])
+    try:
+        from htmldocx import Htmldocx              # 旧版 API
+        h = Htmldocx()
+        h.add_html(html)
+        h.doc.save(sys.argv[2])
+    except ImportError:
+        from htmldocx import HtmlToDocx            # 新版 API
+        doc = Document()
+        HtmlToDocx().add_html_to_document(html, doc)
+        doc.save(sys.argv[2])
     print('DOCX SAVED:', sys.argv[2])
 
 if __name__ == '__main__':
